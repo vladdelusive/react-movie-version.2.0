5c71b66130cda8939881c929bcf5625a4e512443
Vlad Tovstochub <mr.tovstochub@mail.ru>
2020-06-02 00:01:13 +0300

    add: header search img

--- src/components/Header/FormSearch/FormSearch.js
+++ src/components/Header/FormSearch/FormSearch.js
@@ -1,6 +1,22 @@
-import React from "react"
-import './FormSearch.css'
+import React, { useState } from "react";
+import "./FormSearch.css";
 
 export default function FormSearch() {
-    return <input className="form" placeholder="search" type="text"></input>
+  const [inputClass, setInputClass] = useState("input__search");
+  const [btnClass, setBtnClass] = useState("input__btn");
+
+  const onClick = () => {
+    setInputClass((prev) => {
+      return prev.length > 13 ? prev.slice(0, 13) : `${prev} input__search--clicked`;
+    });
+    setBtnClass((prev) => {
+      return prev.length > 10 ? prev.slice(0, 10) : `${prev} input__btn--clicked`;
+    });
+  };
+  return (
+    <form className="input">
+      <input type="text" name="input" className={inputClass} />
+      <button type="button" className={btnClass} onClick={onClick} />
+    </form>
+  );
 }
