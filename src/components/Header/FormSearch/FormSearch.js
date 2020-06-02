7485ddaf391e8896236341faaaf92e2bbaea2c3a
vladdelusive <tovstochub.v.wezom@gmail.com>
2020-06-02 18:08:18 +0300

    add: search btn

--- src/components/Header/FormSearch/FormSearch.js
+++ src/components/Header/FormSearch/FormSearch.js
@@ -5,7 +5,8 @@ export default function FormSearch() {
     const [classes, setClasses] = useState({
         inputClass: "",
         btnClass: "",
-        closeClass: "hide"
+        closeClass: "hide",
+        sendClass: "hide"
     });
     const [inputValue, setInputValue] = useState("")
 
@@ -13,7 +14,8 @@ export default function FormSearch() {
         setClasses({
             inputClass: "input__search--clicked show",
             btnClass: "input__btn--clicked hide",
-            closeClass: ""
+            closeClass: "",
+            sendClass: ""
         })
     };
 
@@ -21,12 +23,14 @@ export default function FormSearch() {
         setClasses({
             inputClass: "",
             btnClass: "",
-            closeClass: "hide"
+            closeClass: "hide",
+            sendClass: "hide"
         })
     };
 
     return (
         <form className="input" onSubmit={(e)=>e.preventDefault()}>
+            <button type="button" className={`${classes.sendClass} input__btn`} onClick={()=>{}}/>
             <input type="text" name="input" className={`${classes.inputClass} input__search`} value={inputValue} onChange={e=>setInputValue(e.target.value)}/>
             <button type="button" className={`${classes.btnClass} input__btn`} onClick={onClickOpenSearch}/>
             <a className={`${classes.closeClass} close`} onClick={onClickCloseSearch}/>
