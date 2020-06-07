6ce6a5cbacee60be8a1a9b8cb7a78ca1804fcdb7
Vlad Tovstochub <mr.tovstochub@mail.ru>
2020-06-07 23:56:14 +0300

    feat: burger menu

--- src/components/BurgerMenu/BurgerMenu.js
+++ src/components/BurgerMenu/BurgerMenu.js
@@ -1,25 +1,35 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
-import './BurgerMenu.css'
+import "./BurgerMenu.css";
+import CloseBurger from '../../images/closeBurger.png'
 
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
+
+import {SEARCH_BURGER} from '../../store/SEARCH/actions/actionTypes'
 
 export default function BurgerMenu() {
-    const {burgerActive} = useSelector(({ search }) => search);
+  const { burgerActive } = useSelector(({ search }) => search);
+  const dispatch = useDispatch()
+  const dispatchAction = () => dispatch({type: SEARCH_BURGER})
   return (
-    <div className={`panel-menu ${burgerActive ? "panel-menu--transform" : ""}`}>
+    <div
+      className={`panel-menu ${burgerActive ? "panel-menu--transform" : ""}`}
+    >
+      <div className="close-burger" onClick={dispatchAction}>
+          <img className="close-burger__icon" src={CloseBurger} alt="close_burger"/>
+      </div>
       <ul className="panel-menu__navbar">
-        <li className="panel-menu__navbar-item" onClick={() => {}}>
+        <li className="panel-menu__navbar-item" onClick={dispatchAction}>
           <NavLink exact className="navbar__link" to="/">
             Main
           </NavLink>
         </li>
-        <li className="panel-menu__navbar-item" onClick={() => {}}>
+        <li className="panel-menu__navbar-item" onClick={dispatchAction}>
           <NavLink className="navbar__link" to="/movies">
             Movies
           </NavLink>
         </li>
-        <li className="panel-menu__navbar-item" onClick={() => {}}>
+        <li className="panel-menu__navbar-item" onClick={dispatchAction}>
           <NavLink className="navbar__link" to="/actors">
             Actors
           </NavLink>
