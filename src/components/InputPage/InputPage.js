3c5d2f03bec487829dfa1e7e241a22698a45895b
vladdelusive <tovstochub.v.wezom@gmail.com>
2020-06-10 11:52:50 +0300

    add: easter egg for my search

--- src/components/InputPage/InputPage.js
+++ src/components/InputPage/InputPage.js
@@ -2,12 +2,14 @@ import React from 'react'
 import './InputPage.css'
 import NoQuery from "./NoQuery/NoQuery";
 import SearchContent from "./SearchContent/SearchContent";
+import EasterEgg from '../EasterEgg/EasterEgg'
 
 function InputPage({location}) {
     if (!location.search) return <NoQuery/>
     const text = decodeURI(location.search.slice(7))
         .replace(/\s+/g, ' ').trim()
     if (!text) return <NoQuery/>
+    if(text.toLowerCase() === "товсточуб" || text.toLowerCase() === "tovstochub") return <EasterEgg/>
 
     return (
         <div className="section__search">
