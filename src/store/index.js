0ec77acfe86b189809d18f62cc9173d214e9bca9
Vlad Tovstochub <mr.tovstochub@mail.ru>
2020-06-19 01:46:15 +0300

    fix: error with devtool

--- src/store/index.js
+++ src/store/index.js
@@ -4,7 +4,10 @@ import { rootReducer } from './root-reducer'
 
 const devTools = process.env.NODE_ENV === 'development' ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__() : null
 
+let enhancer = applyMiddleware(thunk);
+if(devTools) enhancer = compose(applyMiddleware(thunk), devTools)
+
 export const store = createStore(
   rootReducer,
-  compose(applyMiddleware(thunk), devTools)
+  enhancer
 )
