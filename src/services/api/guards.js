0f016e0bfa5cbc847e54f6e46f3722452b5846b8
Vlad Tovstochub <mr.tovstochub@mail.ru>
2020-06-19 23:06:08 +0300

    remove: less logic in guards

--- src/services/api/guards.js
+++ src/services/api/guards.js
@@ -4,7 +4,6 @@ export const guards = {
         if(Array.isArray(request.data.results)){ 
             return request.data.results
         } else {
-            console.log("REQUEST data is not found in our DB")
             return []
         }
     },
@@ -12,32 +11,24 @@ export const guards = {
         if(Array.isArray(request.data.cast)){
             return request.data.cast
         } else {
-            console.log("REQUEST data is not found in our DB")
             return []
         }
     },
     actorDetails: (request) => {
-        if(request.data instanceof Object ){
-            return request.data
-        } else {
-            console.log("REQUEST data is not found in our DB")
-            return null
-        }
+        return request.data
     },
     movieResults: (request) => {
-        if(request.data instanceof Object && Array.isArray(request.data.results) && request.data.results.length !== 0){
+        if(Array.isArray(request.data.results) && request.data.results.length !== 0){
             return request.data.results
         } else {
-            console.log("REQUEST data is not found in our DB")
             return []
         }
     },
     searchData: ({data}) => {
-        if(data instanceof Object && Array.isArray(data.results) && data.results.length !== 0){
+        if(Array.isArray(data.results) && data.results.length !== 0){
             if(typeof data.total_results === 'number' && typeof data.total_pages === 'number' && typeof data.page === 'number') {
                 return data
             }
-            console.log("REQUEST data is not found in our DB")
             return {
                 page: 1,
                 total_results: 0, 
@@ -45,7 +36,6 @@ export const guards = {
                 results: []
             }
         } else {
-            console.log("REQUEST data is not found in our DB")
             return {
                 page: 1,
                 total_results: 0, 
